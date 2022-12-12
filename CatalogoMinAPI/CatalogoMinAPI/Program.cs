15770e340998a38ff912efae42683abc26fa7bf2
Rafael Candeias <rafaelcandeias39@gmail.com>
2022-12-12 16:40:32 -0300

    @RafaelCandeias00 [Task: add métodos GET, POST, PUT e DELETE | Categorias]

--- CatalogoMinAPI/CatalogoMinAPI/Program.cs
+++ CatalogoMinAPI/CatalogoMinAPI/Program.cs
@@ -16,8 +16,19 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 
 var app = builder.Build();
 
-// Denifindo Endpoints
-app.MapGet("/", () => "Catálogo de Produtos - 2022");
+// -- Denifindo Endpoints --
+
+// -- Categorias
+app.MapGet("/", () => "Catálogo de Produtos - 2022").ExcludeFromDescription();
+
+app.MapGet("/categorias", async (AppDbContext db) =>
+    await db.Categorias.ToListAsync());
+
+app.MapGet("/categorias/{id:int}", async (int id, AppDbContext db) =>
+{
+    return await db.Categorias.FindAsync(id) 
+        is Categoria categoria ? Results.Ok(categoria) : Results.NotFound("Categoria não encontrado!");
+});
 
 app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>
 {
@@ -37,6 +48,38 @@ app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>
   .Produces<Categoria>(StatusCodes.Status201Created)
   .WithName("CriarNovaCategoria")
   .WithTags("Setter");*/
+
+app.MapPut("/categorias/{id:int}", async(int id, Categoria categoria, AppDbContext db) =>
+{
+    if(categoria.CategoriaId != id)
+    {
+        return Results.BadRequest();
+    }
+
+    var categoriaDB = await db.Categorias.FindAsync(id);
+
+    if (categoriaDB is null) return Results.NotFound();
+
+    categoriaDB.Nome = categoria.Nome;
+    categoriaDB.Descricao = categoria.Descricao;
+
+    await db.SaveChangesAsync();
+    return Results.Ok(categoriaDB);
+});
+
+app.MapDelete("/categorias/{id:int}", async (int id, AppDbContext db) =>
+{
+    var categoria = await db.Categorias.FindAsync(id);
+
+    if(categoria is null)
+    {
+        return Results.NotFound();
+    }
+
+    db.Categorias.Remove(categoria);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
 
 // -- Configure --
 if (app.Environment.IsDevelopment())
