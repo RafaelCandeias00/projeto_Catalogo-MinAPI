755035c423b8cfbd3fbd9381ca45da4b69c6c4e2
Rafael Candeias <rafaelcandeias39@gmail.com>
2022-12-12 18:39:18 -0300

    @RafaelCandeias00 [Task: add segurança com JWT]

--- CatalogoMinAPI/CatalogoMinAPI/Program.cs
+++ CatalogoMinAPI/CatalogoMinAPI/Program.cs
@@ -1,20 +1,100 @@
 using CatalogoMinAPI.Context;
 using CatalogoMinAPI.Models;
+using CatalogoMinAPI.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Diagnostics.Metrics;
+using System.Reflection.Metadata;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // -- Configure Service --
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalogo MinApi", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = @"JWT Authorization header using the Bearer scheme.
+                   \r\n\r\n Enter 'Bearer'[space] and then your token in the text input below.
+                    \r\n\r\nExample: \'Bearer 12345abcdef\'",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                          {
+                              Reference = new OpenApiReference
+                              {
+                                  Type = ReferenceType.SecurityScheme,
+                                  Id = "Bearer"
+                              }
+                          },
+                         new string[] {}
+                    }
+                });
+});
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(opt => 
     opt.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)
 ));
 
+builder.Services.AddSingleton<ITokenService>(new TokenService());
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                                    .AddJwtBearer(opt => {
+                                        opt.TokenValidationParameters = new TokenValidationParameters
+                                        {
+                                            ValidateIssuer = true,
+                                            ValidateAudience = true,
+                                            ValidateLifetime = true,
+                                            ValidateIssuerSigningKey = true,
+
+                                            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                                            ValidAudience = builder.Configuration["Jwt:Audience"],
+                                            IssuerSigningKey = new SymmetricSecurityKey
+                                            (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                                        };
+                                    });
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+// Endpoint para login
+app.MapPost("/login", [AllowAnonymous] (UserModel userModel, ITokenService tokenService) =>
+{
+    if (userModel == null)
+    {
+        return Results.BadRequest("Login Inválido!");
+    }
+    if (userModel.Username == "rafael" && userModel.Password == "num123")
+    {
+        var tokenString = tokenService.GerarToken(app.Configuration["Jwt:Key"],
+            app.Configuration["Jwt:Issuer"],
+            app.Configuration["Jwt:Audience"],
+            userModel);
+        return Results.Ok(new { token = tokenString });
+    }
+    else
+    {
+        return Results.BadRequest("Login inválido!");
+    }
+}).Produces(StatusCodes.Status400BadRequest)
+    .Produces(StatusCodes.Status200OK)
+    .WithName("Login")
+    .WithTags("Autenticacao");
 
 // -- Denifindo Endpoints --
 
@@ -22,13 +102,13 @@ var app = builder.Build();
 app.MapGet("/", () => "Catálogo de Produtos - 2022").ExcludeFromDescription();
 
 app.MapGet("/categorias", async (AppDbContext db) =>
-    await db.Categorias.ToListAsync());
+    await db.Categorias.ToListAsync()).WithTags("Categorias").RequireAuthorization();
 
 app.MapGet("/categorias/{id:int}", async (int id, AppDbContext db) =>
 {
     return await db.Categorias.FindAsync(id) 
         is Categoria categoria ? Results.Ok(categoria) : Results.NotFound("Categoria não encontrado!");
-});
+}).WithTags("Categorias");
 
 app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>
 {
@@ -36,7 +116,7 @@ app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>
     await db.SaveChangesAsync();
 
     return Results.Created($"/categorias/{categoria.CategoriaId}", categoria);
-});
+}).WithTags("Categorias");
 // Código Alternativo
 /*app.MapPost("/categorias", async ([FromBody] Categoria categoria, [FromServices] AppDbContext db) =>
 {
@@ -65,7 +145,7 @@ app.MapPut("/categorias/{id:int}", async(int id, Categoria categoria, AppDbConte
 
     await db.SaveChangesAsync();
     return Results.Ok(categoriaDB);
-});
+}).WithTags("Categorias");
 
 app.MapDelete("/categorias/{id:int}", async (int id, AppDbContext db) =>
 {
@@ -79,17 +159,17 @@ app.MapDelete("/categorias/{id:int}", async (int id, AppDbContext db) =>
     db.Categorias.Remove(categoria);
     await db.SaveChangesAsync();
     return Results.NoContent();
-});
+}).WithTags("Categorias");
 
 // -- Produto
 app.MapGet("/produtos", async (AppDbContext db) => 
-    await db.Produtos.ToListAsync());
+    await db.Produtos.ToListAsync()).WithTags("Produtos").RequireAuthorization();
 
 app.MapGet("/produtos/{id:int}", async (int id, AppDbContext db) =>
 {
     return await db.Produtos.FindAsync(id)
         is Produto produto ? Results.Ok(produto) : Results.NotFound("Produto não encontrado!");
-});
+}).WithTags("Produtos");
 
 app.MapPost("/produtos", async(Produto produto, AppDbContext db) =>
 {
@@ -97,7 +177,7 @@ app.MapPost("/produtos", async(Produto produto, AppDbContext db) =>
     await db.SaveChangesAsync();
 
     return Results.Created($"/produtos/{produto.ProdutoId}", produto);
-});
+}).WithTags("Produtos");
 
 app.MapPut("/produtos/{id:int}", async (int id, Produto produto, AppDbContext db) =>
 {
@@ -120,7 +200,7 @@ app.MapPut("/produtos/{id:int}", async (int id, Produto produto, AppDbContext db
 
     await db.SaveChangesAsync();
     return Results.Ok(produtoDB);
-});
+}).WithTags("Produtos");
 
 app.MapDelete("/produtos/{id:int}", async (int id, AppDbContext db) =>
 {
@@ -134,7 +214,7 @@ app.MapDelete("/produtos/{id:int}", async (int id, AppDbContext db) =>
     db.Produtos.Remove(produto);
     await db.SaveChangesAsync();
     return Results.NoContent();
-});
+}).WithTags("Produtos");
 
 // -- Configure --
 if (app.Environment.IsDevelopment())
@@ -142,4 +222,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.Run();
---
+++ CatalogoMinAPI/CatalogoMinAPI/Services/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using CatalogoMinAPI.Models;
+
+namespace CatalogoMinAPI.Services
+{
+    public interface ITokenService
+    {
+        string GerarToken(string key, string issuer, string audience, UserModel user);
+    }
+}
