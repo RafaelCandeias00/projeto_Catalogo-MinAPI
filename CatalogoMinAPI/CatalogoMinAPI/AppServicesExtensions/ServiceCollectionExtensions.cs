fd447b4119fa4454454c6e9c0f0b401812eb673b
Rafael Candeias <rafaelcandeias39@gmail.com>
2022-12-12 19:42:48 -0300

    @RafaelCandeias00 [Task: definindo métodos de extensão]

---
+++ CatalogoMinAPI/CatalogoMinAPI/AppServicesExtensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,91 @@
+﻿using CatalogoMinAPI.Context;
+using CatalogoMinAPI.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+namespace CatalogoMinAPI.AppServicesExtensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        // Criar e configurar serviço do swagger incluindo no container nativo DI
+        public static WebApplicationBuilder AddApiSwagger(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwagger();
+            return builder;
+        }
+        
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalogo MinApi", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = @"JWT Authorization header using the Bearer scheme.
+                   \r\n\r\n Enter 'Bearer'[space] and then your token in the text input below.
+                    \r\n\r\nExample: \'Bearer 12345abcdef\'",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                          {
+                              Reference = new OpenApiReference
+                              {
+                                  Type = ReferenceType.SecurityScheme,
+                                  Id = "Bearer"
+                              }
+                          },
+                         new string[] {}
+                    }
+                });
+            });
+            return services;
+        }
+
+        // Definir persistência do contexto do EF Core e do serviço para gerar o token 
+        public static WebApplicationBuilder AddPersistence(this WebApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<AppDbContext>(opt =>
+                opt.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)
+            ));
+            builder.Services.AddSingleton<ITokenService>(new TokenService());
+
+            return builder;
+        }
+
+        // Definir autenticação e autorização do token JWT
+        public static WebApplicationBuilder AddAutenticationJwt(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                                    .AddJwtBearer(opt => {
+                                        opt.TokenValidationParameters = new TokenValidationParameters
+                                        {
+                                            ValidateIssuer = true,
+                                            ValidateAudience = true,
+                                            ValidateLifetime = true,
+                                            ValidateIssuerSigningKey = true,
+
+                                            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                                            ValidAudience = builder.Configuration["Jwt:Audience"],
+                                            IssuerSigningKey = new SymmetricSecurityKey
+                                            (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                                        };
+                                    });
+            builder.Services.AddAuthorization();
+
+            return builder;
+        }
+    }
+}
