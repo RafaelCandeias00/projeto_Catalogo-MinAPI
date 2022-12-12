ccad5abd86e01400eb963c477dccb990763f1fcd
Rafael Candeias <rafaelcandeias39@gmail.com>
2022-12-12 14:49:29 -0300

    @RafaelCandeias00 [Task: add arquivo de contexto]

---
+++ CatalogoMinAPI/CatalogoMinAPI/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CatalogoMinAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogoMinAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        DbSet<Produto>? Produtos { get; set; }
+        DbSet<Categoria>? Categorias { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+        }
+    }
+}
